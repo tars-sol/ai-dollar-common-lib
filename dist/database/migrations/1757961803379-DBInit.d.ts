43c779999cafc61cea5d5baef3c97d19cfd63ea4
ahsanahmed321 <ahsan.ahmed.frl@gmail.com>
2025-09-15 23:51:54 +0500

    migrations DB Init updates

--- dist/database/migrations/1757961803379-DBInit.d.ts
+++ dist/database/migrations/1757961803379-DBInit.d.ts
@@ -1,5 +1,5 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
-export declare class DBInit1757454456311 implements MigrationInterface {
+export declare class DBInit1757961803379 implements MigrationInterface {
     name: string;
     up(queryRunner: QueryRunner): Promise<void>;
     down(queryRunner: QueryRunner): Promise<void>;
