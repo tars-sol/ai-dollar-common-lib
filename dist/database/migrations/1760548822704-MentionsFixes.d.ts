70cbd96ddf8d7e73b8f8ac9bd2d60abfd657d8c7
ahsanahmed321 <ahsan.ahmed.frl@gmail.com>
2025-10-16 17:26:41 +0500

    MentionFixes migration remove

--- dist/database/migrations/1760548822704-MentionsFixes.d.ts
+++
@@ -1,6 +0,0 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
-export declare class MentionsFixes1760548822704 implements MigrationInterface {
-    name: string;
-    up(queryRunner: QueryRunner): Promise<void>;
-    down(queryRunner: QueryRunner): Promise<void>;
-}
